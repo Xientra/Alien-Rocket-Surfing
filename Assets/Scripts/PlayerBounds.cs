09e68d6519d652027d386f5861d768ffc293897a
Xientra <38855570+Xientra@users.noreply.github.com>
2020-08-14 21:16:59 +0200

    Added unity project

---
+++ Assets/Scripts/PlayerBounds.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerBounds : MonoBehaviour {
+
+	public Vector3 cornerTopLeft;
+	public Vector3 cornerTopRight;
+	public Vector3 cornerBottomLeft;
+	public Vector3 cornerBottomRight;
+
+	public float scaleX;
+	public float scaleY;
+
+	public float boundX;
+	public float boundY;
+
+	private void OnDrawGizmos() {
+		scaleX = transform.localScale.x / 2;
+		scaleY = transform.localScale.y / 2;
+
+		//boundX = transform.position.x + 
+
+		cornerTopLeft = transform.position + new Vector3(-scaleX, scaleY, 0);
+		cornerTopRight = transform.position + new Vector3(scaleX, scaleY, 0);
+		cornerBottomLeft = transform.position + new Vector3(-scaleX, -scaleY, 0);
+		cornerBottomRight = transform.position + new Vector3(scaleX, -scaleY, 0);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(cornerTopLeft, cornerBottomLeft);
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(cornerTopRight, cornerBottomRight);
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(cornerTopLeft, cornerTopRight);
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(cornerBottomLeft, cornerBottomRight);
+	}
+
+	//private void OnValidate() {
+	//}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	private PlayerBounds playerBounds;
+
+	[SerializeField]
+	private float boundsMultiplier = 0.9f;
+
+	[SerializeField]
+	private Vector2 speed = new Vector2(1, 1);
+
+	private float horizontalInput;
+	private float verticalInput;
+
+	private Rigidbody2D rb;
+	private Camera cam;
+
+
+	private void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Start() {
+		cam = Camera.main;
+	}
+
+	private void Update() {
+		horizontalInput = Input.GetAxis("Horizontal");
+		verticalInput = Input.GetAxis("Vertical");
+
+	}
+
+	private void FixedUpdate() {
+
+		float xBounds = playerBounds.scaleX * boundsMultiplier;
+		float yBounds = playerBounds.scaleY * boundsMultiplier;
+
+		float ySpeed = 0;
+		float xSpeed = 0;
+
+		if (horizontalInput < 0) {
+			if (transform.position.x > -xBounds)
+				xSpeed = speed.x * horizontalInput;
+		}
+		else if (horizontalInput > 0) {
+			if (transform.position.x < xBounds)
+				xSpeed = speed.x * horizontalInput;
+		}
+
+		if (verticalInput < 0) {
+			if (transform.position.y > -yBounds)
+				ySpeed = speed.y * verticalInput;
+		}
+		else if (verticalInput > 0) {
+			if (transform.position.y < yBounds)
+				ySpeed = speed.y * verticalInput;
+		}
+
+		rb.MovePosition(transform.position + new Vector3(xSpeed, ySpeed, 0));
+	}
+}
