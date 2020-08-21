2529f87ba6ca33979248edfb4f7a561be4acc244
Xientra <38855570+Xientra@users.noreply.github.com>
2020-08-21 12:09:25 +0200

    The player now has a thruster that pushes in the viewing direction and the basics of ramps is now in place

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed = 0.5f;
+
+	public Vector3[] points = new Vector3[0];
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+
+	public bool InBounds(float x) {
+		return (x >= (transform.position + points[0]).x && x <= (transform.position + points[points.Length - 1]).x);
+	}
+
+	public float Evaluate(float x) {
+		if (InBounds(x) == false)
+			throw new ArgumentOutOfRangeException("x", "Call InBounds(float x) before calling Evaluate(float x)");
+		
+		for (int i = 1; i < points.Length; i++) {
+			Vector3 p1 = points[i - 1] + transform.position;
+			Vector3 p2 = points[i] + transform.position;
+
+			if (x <= p2.x) {
+				float t = 1 - (p2.x - x) / (p2.x - p1.x);
+				//Debug.Log("r: " + r + " i: " + i + " t: " + t);
+
+				return Vector3.Lerp(p1, p2, t).y;
+			}
+		}
+
+		throw new ArgumentOutOfRangeException("x", "x was out of bounds, call InBounds(float x) before calling Evaluate(float x)");
+	}
+
+	private void FixedUpdate() {
+		transform.Translate(-transform.right * moveSpeed);
+	}
+
+	private void OnDrawGizmos() {
+
+		for (int i = 1; i < points.Length; i++) { 
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawLine(transform.position + points[i - 1], transform.position + points[i]);
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour {
@@ -13,6 +15,11 @@ public class PlayerMovement : MonoBehaviour {
 
 	private Rigidbody2D rb;
 
+	// input
+	private float horizontalInput;
+	private float verticalInput;
+	private float rotationInput;
+	private bool jumpInput;
 
 
 
@@ -22,16 +29,19 @@ public class PlayerMovement : MonoBehaviour {
 	[SerializeField]
 	private float rotationSpeed = 3f;
 
+	[Header("Vertical:")]
+
 	[SerializeField]
 	private float gravity = -1;
 
+
+	[SerializeField]
+	private float rocketStrength = 2f;
+
+	[Header("Jumping:")]
+	public bool canJump = true;
+
 	private bool grounded = false;
-
-
-	private float horizontalInput;
-	private float verticalInput;
-	private float rotationInput;
-	private bool jumpInput;
 
 	[SerializeField]
 	private float jumpAmplitude = 1;
@@ -39,8 +49,13 @@ public class PlayerMovement : MonoBehaviour {
 	private float jumpSpeed = 2;
 	private bool jumping = false;
 	private float jumpTime = 0;
-	private float originalY;
 
+
+
+	// ground variables
+	private Obstacle collidingObs = null;
+	private float groundHeight;
+	private float groundRotation;
 
 
 	private void Awake() {
@@ -54,7 +69,6 @@ public class PlayerMovement : MonoBehaviour {
 		horizontalInput = Input.GetAxis("Horizontal");
 		verticalInput = Input.GetAxis("Vertical");
 		rotationInput = Input.GetAxisRaw("Rotation");
-		Debug.Log(rotationInput);
 		jumpInput = Input.GetAxis("Jump") != 0;
 		if (jumpInput && jumping == false && grounded == true) {
 			Jump();
@@ -62,15 +76,14 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	private void FixedUpdate() {
-		float xPos = transform.position.x;
-		float yPos = transform.position.y;
+
+		Vector3 newPos = transform.position;
 
 		float speedX = !grounded ? speed.x / 2 : speed.x;
 
-		// vertical movement
+		// jumping
 		if (jumping == true) {
-			// jumping
-			yPos += JumpingCurve(jumpTime);
+			newPos.y += JumpingCurve(jumpTime);
 
 			jumpTime += Time.fixedDeltaTime;
 
@@ -79,37 +92,52 @@ public class PlayerMovement : MonoBehaviour {
 		}
 
 		// gravity
-		yPos -= gravity;
-		if (yPos <= bounds.height) {
-			yPos = bounds.height;
+		newPos.y -= gravity;
+
+		// ground collision
+		groundHeight = bounds.height;
+		if (collidingObs != null && collidingObs.InBounds(transform.position.x)) {
+			float obsHeight = collidingObs.Evaluate(transform.position.x);
+			if (obsHeight > groundHeight)
+				groundHeight = obsHeight;
+		}
+
+		if (newPos.y <= groundHeight) {
+			newPos.y = groundHeight;
 			grounded = true;
 		}
 
 
 		// horizontal movement
-		if (horizontalInput < 0) {
-			xPos = transform.position.x + speedX * horizontalInput;
-			if (xPos < bounds.left)
-				xPos = bounds.left;
-		}
-		else if (horizontalInput > 0) {
-			xPos = transform.position.x + speedX * horizontalInput;
-			if (xPos > bounds.right)
-				xPos = bounds.right;
-		}
+		if (horizontalInput < 0)
+			newPos.x = transform.position.x + speedX * horizontalInput;
+		else if (horizontalInput > 0)
+			newPos.x = transform.position.x + speedX * horizontalInput;
 
 		// rotation
 		transform.Rotate(gfx.transform.forward, rotationInput * rotationSpeed * Time.fixedDeltaTime);
 
+		// rocket thrust
+		newPos.y += (transform.right * rocketStrength).y;
+
+
+		// horizontal bounds
+		if (newPos.x < bounds.left)
+			newPos.x = bounds.left;
+		if (newPos.x > bounds.right)
+			newPos.x = bounds.right;
+
+
 		// set next position
-		rb.MovePosition(new Vector3(xPos, yPos, 0));
+		rb.MovePosition(new Vector3(newPos.x, newPos.y, 0));
 	}
 
 	public void Jump() {
-		jumpTime = 0;
-		jumping = true;
-		originalY = transform.position.y;
-		grounded = false;
+		if (canJump) {
+			jumpTime = 0;
+			jumping = true;
+			grounded = false;
+		}
 	}
 
 	private float JumpingCurve(float jumpingTime) {
@@ -122,5 +150,17 @@ public class PlayerMovement : MonoBehaviour {
 				Jump();
 			}
 		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		Obstacle obs = collision.GetComponent<Obstacle>();
+		if (obs != null)
+			collidingObs = obs;
+	}
+
+	private void OnTriggerExit2D(Collider2D collision) {
+		Obstacle obs = collision.GetComponent<Obstacle>();
+		if (obs == collidingObs)
+			collidingObs = null;
 	}
 }
